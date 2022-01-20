7ae3dcd2d0618c9377744a289615aa01c26d2fcf
FREEDOM <freewhynane62@gmail.com>
2022-01-20 05:40:21 +0000

    add firebase config

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBSZc3IIrXxW_1t7eOfIJHZnkm_O6byEeI",
+  authDomain: "clone-d38e1.firebaseapp.com",
+  projectId: "clone-d38e1",
+  storageBucket: "clone-d38e1.appspot.com",
+  messagingSenderId: "725300306754",
+  appId: "1:725300306754:web:f7650862a8aada1b51dedb"
+};
