293dc205b19185f018702511756d59ad06ab7f71
FREEDOM <freewhynane62@gmail.com>
2022-01-22 07:00:58 +0000

    add auth and db

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,19 @@
-// eslint-disable-next-line no-unused-vars
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBSZc3IIrXxW_1t7eOfIJHZnkm_O6byEeI",
   authDomain: "clone-d38e1.firebaseapp.com",
   projectId: "clone-d38e1",
   storageBucket: "clone-d38e1.appspot.com",
   messagingSenderId: "725300306754",
-  appId: "1:725300306754:web:f7650862a8aada1b51dedb"
+  appId: "1:725300306754:web:f7650862a8aada1b51dedb",
 };
+
+const firebasApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebasApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
