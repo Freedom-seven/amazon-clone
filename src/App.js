7b89b5c6e3e05ea7d596701b30d5f97491726e6e
FREEDOM <freewhynane62@gmail.com>
2022-01-22 06:58:51 +0000

    add use effect

--- src/App.js
+++ src/App.js
@@ -1,18 +1,57 @@
-import React from "react";
+/* eslint-disable no-sequences */
+import React, { useEffect } from "react";
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Checkout from "./Checkout";
+import Login from "./Login";
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
 
 function App() {
+  const [dispatch] = useStateValue();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      console.log("USER IS HERE >>>>", authUser);
+      if (authUser) {
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        console.log("User has logged out");
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, [dispatch]);
   return (
     <Router>
       <div className="app">
-        <Header />
         <Routes>
-          <Route path="/checkout" element={<Checkout />}></Route>
-          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route
+            path="/checkout"
+            element={
+              <>
+                <Header />
+                <Checkout />
+              </>
+            }
+          ></Route>
+          <Route
+            path="/"
+            element={
+              <>
+                <Header />
+                <Home />
+              </>
+            }
+          ></Route>
         </Routes>
       </div>
     </Router>
