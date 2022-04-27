ee557c29fc9b89c8e0098dea16b27e111890944a
FREEDOM <freewhynane62@gmail.com>
2022-04-27 07:10:50 +0000

    mordify

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -3,40 +3,40 @@ import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
 import { useStateValue } from "./StateProvider";
 import { getBasketTotal } from "./Reducer";
-<<<<<<< HEAD
-
-function Subtotal() {
-  const [{ basket }] = useStateValue();
-=======
 import { useNavigate } from "react-router-dom";
 
 function Subtotal() {
-  const navigate = useNavigate();
-  const [{ basket }, dispatch] = useStateValue();
->>>>>>> developer
-  return (
-    <div className="subtotal">
-      <CurrencyFormat
-        renderText={(value) => (
-          <>
-            <p>
-              Subtotal ({basket.length} items): <strong>{value}</strong>
-            </p>
-            <small className="subtotal__gift">
-              <input type="checkbox" /> This order contains a gift
-            </small>
-          </>
-        )}
-        decimalScale={2}
-        value={getBasketTotal(basket)}
-        displayType={"text"}
-        thousandSeparator={true}
-        prefix={"$"}
-      />
+  const [{ basket }] = useStateValue();
 
-      <button onClick={(e) => navigate("/payment")}>Proceed to Checkout</button>
-    </div>
-  );
+  function Subtotal() {
+    const navigate = useNavigate();
+    const [{ basket }, dispatch] = useStateValue();
+    return (
+      <div className="subtotal">
+        <CurrencyFormat
+          renderText={(value) => (
+            <>
+              <p>
+                Subtotal ({basket.length} items): <strong>{value}</strong>
+              </p>
+              <small className="subtotal__gift">
+                <input type="checkbox" /> This order contains a gift
+              </small>
+            </>
+          )}
+          decimalScale={2}
+          value={getBasketTotal(basket)}
+          displayType={"text"}
+          thousandSeparator={true}
+          prefix={"$"}
+        />
+
+        <button onClick={(e) => navigate("/payment")}>
+          Proceed to Checkout
+        </button>
+      </div>
+    );
+  }
 }
 
 export default Subtotal;
